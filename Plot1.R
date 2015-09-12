57e29f8dce92b219bd317e5741ef2099ba677e6b
Kirjanovas <Kirjanovas@yandex.ru>
2015-09-12 14:14:34 +0300

    Project 1 Homework
    Including 8 files: 4 with R code and 4 as results of executing of code
    in PNG format

---
+++ Plot1.R
@@ -0,0 +1,34 @@
+setwd("E:/2015/Exploratory Data Analysis/Lections/Project 1")# Please, set the correct path to source of data
+library(lubridate)
+Sys.setlocale("LC_TIME", "USA")
+
+fn<-"household_power_consumption.txt"
+
+DF <- read.table(pipe("findstr /B /R ^[1-2]/2/2007 household_power_consumption.txt"),sep=";")
+dates <- as.character(DF$V1)
+times <- (DF$V2)
+x <- paste(dates,times, sep =" ")
+z<-dmy_hms(x)
+DF[10]<-z
+
+
+DFN <- read.table(pipe("findstr /B /R ^Date household_power_consumption.txt"),sep=";")
+
+a<-as.character(DFN$V1)
+b<-as.character(DFN$V2)
+c<-as.character(DFN$V3)
+d<-as.character(DFN$V4)
+e<-as.character(DFN$V5)
+f<-as.character(DFN$V6)
+g<-as.character(DFN$V7)
+h<-as.character(DFN$V8)
+i<-as.character(DFN$V9)
+Z<-c(a,b,c,d,e,f,g,h,i,"Times")
+
+
+colnames(DF)<-Z
+
+#Plot 1
+hist(DF$Global_active_power, col = "red", main= "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "Plot1.png",width = 480, height = 480,pointsize = 12) ## Copy Plot1 to a PNG file
+dev.off()
---
+++ Plot3.R
@@ -0,0 +1,37 @@
+setwd("E:/2015/Exploratory Data Analysis/Lections/Project 1")# Please, set the correct path to source of data
+library(lubridate)
+Sys.setlocale("LC_TIME", "USA")
+
+fn<-"household_power_consumption.txt"
+
+DF <- read.table(pipe("findstr /B /R ^[1-2]/2/2007 household_power_consumption.txt"),sep=";")
+dates <- as.character(DF$V1)
+times <- (DF$V2)
+x <- paste(dates,times, sep =" ")
+z<-dmy_hms(x)
+DF[10]<-z
+
+
+DFN <- read.table(pipe("findstr /B /R ^Date household_power_consumption.txt"),sep=";")
+
+a<-as.character(DFN$V1)
+b<-as.character(DFN$V2)
+c<-as.character(DFN$V3)
+d<-as.character(DFN$V4)
+e<-as.character(DFN$V5)
+f<-as.character(DFN$V6)
+g<-as.character(DFN$V7)
+h<-as.character(DFN$V8)
+i<-as.character(DFN$V9)
+Z<-c(a,b,c,d,e,f,g,h,i,"Times")
+
+
+colnames(DF)<-Z
+#Plot 3
+ 
+with(DF,plot(Times,Sub_metering_1,type='l',ylab = "Energy sub metering",xlab = ""))
+with(DF,lines(Times,Sub_metering_2,type='l',col = "red"))
+with(DF,lines(Times,Sub_metering_3,type='l',col = "blue"))
+legend("topright", pch = "", lwd=1,col = c("black","red", "blue"),text.width = 60000 ,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.copy(png, file = "Plot3.png",width = 480, height = 480,pointsize = 12) ## Copy Plot3 to a PNG file
+dev.off()
